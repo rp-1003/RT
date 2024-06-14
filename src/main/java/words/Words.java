5e96ce25e66b5895cb0446427a97effd1fe47d68
roopapatil <34306842+rp-1003@users.noreply.github.com>
2024-06-14 17:00:01 +0100

    Create Words.java

---
+++ src/main/java/words/Words.java
@@ -0,0 +1,8 @@
+package words;
+
+import java.util.List;
+
+public class Words {
+    public static List<String> getUniqueWordsFromSentence(String sentence) {
+        throw new UnsupportedOperationException();
+  
